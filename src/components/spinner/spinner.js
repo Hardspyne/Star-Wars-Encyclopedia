62a19d2452ec5b2ad5aea0476223227061db6db2
Hardspyne <79159707922@yandex.ru>
2019-09-28 18:03:45 +0300

    add loader

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './spiner.css';
+
+const Spinner = () => {
+
+    return (
+        <div className="lds-css ng-scope">
+            <div className="lds-double-ring">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Spinner;
+
