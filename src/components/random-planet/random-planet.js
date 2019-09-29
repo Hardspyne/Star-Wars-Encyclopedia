c6aaced8a0d5ebed6d79d4cff3672de311cfdf10
Hardspyne <79159707922@yandex.ru>
2019-09-29 10:54:38 +0300

    make random-planet component

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,36 +1,101 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import './random-planet.css';
+import SwApiService from '../../services/swapi-service';
+import Spinner from "../spinner";
+import ErrorIndicator from "../error-indicator";
+import defaultImage from './planet-default.jpg';
+
 
 export default class RandomPlanet extends Component {
 
+    swApiService = new SwApiService();
+
+
+    constructor(props) {
+        super(props);
+        this.updatePlanet();
+        window.setInterval(this.updatePlanet, 5000);
+    }
+
+    state = {
+        loading: true,
+        hasError: false,
+        planet: {}
+    };
+
+    updatePlanet = async () => {
+        try {
+            const count = await this.swApiService.getAllPlanetsCount();
+            const id = Math.floor(Math.random() * count);
+            const planet = await this.swApiService.getPlanet(id);
+            this.onPlanetLoaded(planet);
+        } catch (e) {
+            this.onError(e);
+        }
+
+    };
+
+    onPlanetLoaded = (planet) => {
+        this.setState({planet, loading: false, hasError: false})
+
+    };
+
+    onError = (error) => {
+        this.setState({hasError: true})
+    };
+
     render() {
+        const {planet, loading, hasError} = this.state;
+
+        let errorIndicator = null;
+        let spinner = null;
+        let planetView = null;
+
+        if (hasError) {
+            errorIndicator = <ErrorIndicator/>;
+        } else if (loading) {
+            spinner = <Spinner/>
+        } else {
+            planetView = <PlanetVieW planet={planet}/>
+        }
+
 
         return (<div className='media random-planet'>
-                <img src="https://starwars-visualguide.com/assets/img/planets/5.jpg" alt='alt text'/>
-                <div className='media-body'>
-                    <h4>Planet name</h4>
-                    <ul className='list-group-flush'>
-                        <li className='list-group-item'>
-                            Population
-                            <span className='value'>
-                                123124
-                            </span>
-                        </li>
-                        <li className='list-group-item'>
-                            Rotation Period
-                            <span className='value'>
-                                43
-                            </span>
-                        </li>
-                        <li  className='list-group-item'>
-                            Diameter
-                            <span className='value'>
-                                100
-                            </span>
-                        </li>
-                    </ul>
-                </div>
+                {errorIndicator}
+                {spinner}
+                {planetView}
             </div>
         );
     }
 }
+
+const PlanetVieW = ({planet: {imageUrl, name, population, rotationPeriod, diameter}} = {}) => {
+    return (
+        <Fragment>
+            <img src={imageUrl == null ? defaultImage : imageUrl} alt='alt text'/>
+            <div className='media-body'>
+                <h4>{name}</h4>
+                <ul className='list-group-flush'>
+                    <li className='list-group-item'>
+                        Population
+                        <span className='value'>
+                                    {population}
+                            </span>
+                    </li>
+                    <li className='list-group-item'>
+                        Rotation Period
+                        <span className='value'>
+                                {rotationPeriod}
+                            </span>
+                    </li>
+                    <li className='list-group-item'>
+                        Diameter
+                        <span className='value'>
+                                {diameter}
+                            </span>
+                    </li>
+                </ul>
+            </div>
+        </Fragment>
+    )
+};
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,6 +1,7 @@
 export default class SwApiService {
 
     _apiBase = 'https://swapi.co/api';
+    _imageBase = 'https://starwars-visualguide.com/assets/img';
 
     async getResource(url) {
         const res = await fetch(`${this._apiBase}${url}`);
@@ -10,35 +11,104 @@ export default class SwApiService {
         return await res.json();
     };
 
-    async getAllPeople() {
-        const peoples = await this.getResource(`/people`);
-        return peoples.results;
+    async getAllPerson() {
+        const persons = await this.getResource(`/people`);
+        return persons.results.map(SwApiService._convertToPerson);
     }
 
 
-    getPerson(id) {
-        return this.getResource(`/people/${id}`);
+    async getPerson(id) {
+        const result = await this.getResource(`/people/${id}`);
+        return await SwApiService._convertToPerson(result);
     }
 
 
     async getAllPlanets() {
         const planets = await this.getResource(`/planets`);
-        return planets.results;
+        return planets.results.map(this._convertToPlanet);
     }
 
 
-    getPlanet(id) {
-        return this.getResource(`/planets/${id}`);
+    async getAllPlanetsCount() {
+        const planets = await this.getResource(`/planets`);
+        return planets.count;
+    }
+
+
+    async getPlanet(id) {
+        const result = await this.getResource(`/planets/${id}`);
+        return await this._convertToPlanet(result);
     }
 
     async getAllStarShips() {
         const starShips = await this.getResource(`/starships`);
-        return starShips.results;
+        return starShips.results.map(SwApiService._convertToStarShip);
     }
 
 
-    getStarShip(id) {
-        return this.getResource(`/starships/${id}`);
+    async getStarShip(id) {
+        const result = await this.getResource(`/starships/${id}`);
+        return await SwApiService._convertToStarShip(result);
+    }
+
+    async _getImageUrlId(id) {
+        const res = await fetch(`${this._imageBase}/planets/${id}.jpg`);
+        if (!res.ok) {
+            return null;
+        } else {
+            return res.url;
+        }
+    }
+
+    static _extractId(from) {
+        const idRegex = /\/([0-9]+)\/$/;
+        return from.url.match(idRegex)[1];
+    }
+
+    async _convertToPlanet(from) {
+        const id = SwApiService._extractId(from);
+        const imageUrl = await this._getImageUrlId(id);
+        return {
+            id,
+            imageUrl,
+            name: from.name,
+            population: from.population,
+            diameter: from.diameter,
+            rotationPeriod: from.rotation_period
+        }
+    }
+
+    static async _convertToStarShip(from) {
+        const id = SwApiService._extractId(from);
+        //todo
+        //  const imageUrl = await this._getImageUrlOrDefaultById(id);
+        return {
+            id,
+            imageUrl: null,
+            name: from.name,
+            model: from.model,
+            manufacturer: from.manufacturer,
+            costInCredits: from.costInCredits,
+            length: from.length,
+            crew: from.crew,
+            passengers: from.passengers,
+            cargoCapacity: from.cargoCapacity
+        }
+    }
+
+
+    static async _convertToPerson(from) {
+        const id = SwApiService._extractId(from);
+        //todo
+        //  const imageUrl = await this._getImageUrlOrDefaultById(id);
+        return {
+            id,
+            imageUrl: null,
+            name: from.name,
+            gender: from.gender,
+            birthYear: from.birthYear,
+            eyeColor: from.eyeColor
+        }
     }
 
 }
